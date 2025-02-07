6d6a0d9bcaf9c5dba58f29a906d4c7acc6f2e85a
abhinesagarwal_deloitte <abhinesagarwal@deloitte.com>
2025-02-07 18:52:30 +0530

    Update the service Discovery enable

--- ServiceDiscovery/src/main/java/com/example/service/discovery/ServiceDiscoveryApplication.java
+++ ServiceDiscovery/src/main/java/com/example/service/discovery/ServiceDiscoveryApplication.java
@@ -1,9 +1,12 @@
 package com.example.service.discovery;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
